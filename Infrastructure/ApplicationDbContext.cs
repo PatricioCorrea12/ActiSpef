45a6e3e59b3f090a7eed56099f41c5e38bfab9ee
Patrick <patriciocorrea_85@hotmail.com>
2020-06-19 18:38:44 -0300

    feat(SpefActi): create seed

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<TeachersIdentity> Teachers { get; set; }
+    }
+}
